d6f96a7940adf5c66286bdfb393d6025672d04dd
Dustin Crawford <crawforddustins@yahoo.com>
2018-10-19 15:15:59 -0500

    Updating code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guess = 0;
 var keyChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var userChoice = "";
 var yourGuesses = [];
@@ -17,22 +18,22 @@ document.onkeyup = function game () {
  
      if (userChoice == keyChoices || userChoice == letter) {
          wins++;
+         userChoice.push(yourGuesses);
     } else {
-        losses++;
+     guess++;
     }
-
     if(userChoice !== keyChoices || userGuess !== letter) {
         for (var i = 0; i < 11; i++) {
-            var guessesLeft = i - losses;
+            var guessesLeft = i - guess;
             
         }
     }
     if (guessesLeft < 0){
     
         guessesLeft = 9;
-        wins = 0;
-        losses = 0;
-        userChoice = [" "];
+        guess = 0;
+        losses++;
+        yourGuesses = [];
     }
 
 
