bcd8c2617055ae1dd71392fa9be2849ddbdff8f4
Dustin Crawford <crawforddustins@yahoo.com>
2018-10-20 22:57:33 -0500

    Finishing up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var guess = 0;
 var keyChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var userChoice = "";
 var yourGuesses = [];
-var isDuplicate = false; 
+
 
 document.onkeyup = function game () {
    
